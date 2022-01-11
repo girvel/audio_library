39491eec685d5a0fca74e8819d3acd273e0eaf8f
girvel <widauka@ya.ru>
2022-01-12 00:51:51 +0500

    Implement generation by a context

--- core/views.py
+++ core/views.py
@@ -4,4 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-  return render(request, 'index.html')
+    return render(request, 'index.html', {
+        'tracks': [
+            {
+                'name': 'Evening Wear',
+                'author': 'Mindless Self Indulgence',
+                'path': 'audio/Mindless Self Indulgence - Evening Wear.mp3'
+            }
+        ]
+    })
