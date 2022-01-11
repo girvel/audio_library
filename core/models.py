97f45fe0d23c29210a516fac834db78f6cab777c
girvel <widauka@ya.ru>
2022-01-12 01:16:50 +0500

    Implement basic DB logic

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Track(models.Model):
+    name = models.CharField(max_length=255, help_text='Track\'s name')
+    author = models.CharField(max_length=255, help_text='Track\'s author')
+    path = models.CharField(max_length=255, help_text='Path to track')
+
+    def __str__(self):
+        return f"{self.author} - {self.name}"
--- core/views.py
+++ core/views.py
@@ -2,14 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Track
+
 
 def index(request):
     return render(request, 'index.html', {
-        'tracks': [
-            {
-                'name': 'Evening Wear',
-                'author': 'Mindless Self Indulgence',
-                'path': 'audio/Mindless Self Indulgence - Evening Wear.mp3'
-            }
-        ]
+        'tracks': Track.objects.all()
     })
