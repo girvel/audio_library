5756aa3c8e273b3cd643a71c988d46301001f9bb
girvel <widauka@ya.ru>
2022-01-16 23:29:20 +0500

    Add 404 exceptions when there is no such artist or track

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 # Create your views here.
@@ -11,12 +11,22 @@ def index(request):
     })
 
 def by_author(request, author_name):
+    tracks = Track.objects.filter(author=author_name)
+
+    if len(tracks) == 0:
+        raise Http404(f'There is no artist named "{author_name}"')
+
     return render(request, 'author.html', {
         'author': author_name,
-        'tracks': Track.objects.filter(author=author_name)
+        'tracks': tracks
     })
 
 def by_name(request, author_name, track_name):
+    tracks = Track.objects.filter(author=author_name, name=track_name)
+
+    if len(tracks) == 0:
+        raise Http404(f'There is no track "{author_name} - {track_name}"')
+
     return render(request, 'track.html', {
         'author': author_name,
         'track': track_name,
