89c3ddf6d65a76ac247da9f8f4f9d6f413a0b1b8
girvel <widauka@ya.ru>
2022-01-16 23:22:11 +0500

    Add basic views
    - `/<author_name>`
    - `/<author_name>/<track_name>`

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<author_name>', views.by_author, name='author'),
+    path('<author_name>/<track_name>', views.by_name, name='track'),
 ]
--- core/views.py
+++ core/views.py
@@ -9,3 +9,16 @@ def index(request):
     return render(request, 'index.html', {
         'tracks': Track.objects.all()
     })
+
+def by_author(request, author_name):
+    return render(request, 'author.html', {
+        'author': author_name,
+        'tracks': Track.objects.filter(author=author_name)
+    })
+
+def by_name(request, author_name, track_name):
+    return render(request, 'track.html', {
+        'author': author_name,
+        'track': track_name,
+        'tracks': Track.objects.filter(author=author_name, name=track_name)
+    })
