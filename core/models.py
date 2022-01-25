a1c2b28b378a22f9aa8428229be7a48297688cc0
girvel <widauka@ya.ru>
2022-01-26 03:24:55 +0500

    Realize audio uploading in admin panel

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Track(models.Model):
     name = models.CharField(max_length=255, help_text='Track\'s name')
     author = models.CharField(max_length=255, help_text='Track\'s author')
-    path = models.CharField(max_length=255, help_text='Path to track')
+    file = models.FileField(upload_to='core/static/audio', help_text='Track file')
 
     def __str__(self):
         return f"{self.author} - {self.name}"
